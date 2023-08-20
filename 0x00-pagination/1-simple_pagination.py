09ca71117aced8cfbd5f9f5762cbf2e8f12e83d2
Ironlaydee <daintiestdolly@gmail.com>
2023-08-20 18:38:44 +0000

    easy numbering

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,31 +1,32 @@
-#!/usr/bin/env python3
-"""A function that defines class Server that paginates a database of popular baby names
-"""
+#!/usr/bin/evn python3
+'''A function that defines class Server that paginates a database of popular baby names'''
+
 import csv
-from typing import List, Tuple
+from typing import list, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
-    start = (page - 1) * page_size
-    end = start + page_size
+    ''' From a given page, collect the index range'''
+
+    start, end = 0, 0
+    for i in range(page):
+        start = end
+        end += page_size
+
     return (start, end)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    ''' Server class to paginate a database of popular baby names'''
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance.
-        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        ''' Cached dataset'''
+
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -35,10 +36,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
-        """
+        ''' Collect a page of dataset'''
+
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
+
         start, end = index_range(page, page_size)
         data = self.dataset()
         if start > len(data):
