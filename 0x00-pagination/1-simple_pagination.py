0025973e198cea046a8d58c8504a1f8b73ef5a9b
Ironlaydee <daintiestdolly@gmail.com>
2023-08-21 17:31:37 +0000

    easy numbering

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" A function that defines class Server that paginates a database of popular baby names
+"""
+Simple helper pagination
 """
 
 import csv
-from typing import List, Tuple
+from typing import List, Dict
 
 
 class Server:
@@ -13,6 +14,7 @@ class Server:
 
     def __init__(self):
         self.__dataset = None
+        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -25,22 +27,40 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ Finds the correct indexes to paginate dataset.
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
         """
-        assert type(page) == int
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Returns a dictionary with key-value pairs
+         (index, next_index, page_size, data)
+        :param index:
+        :param page_size:
+        :return:
+        """
+        assert type(index) == int
         assert type(page_size) == int
-        assert page > 0
-        assert page_size > 0
-        csv_size = len(self.dataset())
-        start, end = index_range(page, page_size)
-        end = min(end, csv_size)
-        if start >= csv_size:
-            return []
-        return self.dataset()[start:end]
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Returns a tuple containing a start and end index.
-    """
-    return ((page - 1) * page_size, page * page_size)
+        csv = self.indexed_dataset()
+        csv_size = len(csv)
+        assert 0 <= index < csv_size
+        data = []
+        _next = index
+        for _ in range(page_size):
+            while not csv.get(_next):
+                _next += 1
+            data.append(csv.get(_next))
+            _next += 1
+        return {
+            "index": index,
+            "data": data,
+            "page_size": page_size,
+            "next_index": _next
+        }
